d26af0ba002e60ef8cb6f0f7739b58500e2b8ac1
uriyay <uriyay@gmail.com>
2020-09-15 00:39:48 +0300

    Update find_max.m

--- MATLAB/find_max.m
+++ MATLAB/find_max.m
@@ -1,9 +1,12 @@
-A = [23 42 37 18 52];
+B = [23 42 37 18 52];
 
-max_val = -inf;
-for i = 1:length(A)
-  if A(i) > max_val
-    max_val = A(i)
-  endif
-endfor
-disp(max_val);
+function max_val = get_max(A)
+  max_val = -inf;
+  for i = 1:length(A)
+    if A(i) > max_val
+      max_val = A(i)
+    endif
+  endfor
+endfunction
+
+disp(get_max(B));
