c49769eccf9ccfa8e1fc4ec8793c2fd0f072db10
uriyay <uriyay@gmail.com>
2020-09-14 20:06:09 +0300

    Update radix_sort.py

--- python/Algorithms/radix_sort.py
+++ python/Algorithms/radix_sort.py
@@ -1,13 +1,16 @@
 import random
 
-def radix_sort(arr, d):
-    for i in range(0, d):
-        arr = sorted(arr, key=lambda x: (x % (10**(i+1))) // (10**i))
+def radix_sort(arr, base, digits):
+    # See https://en.wikipedia.org/wiki/Radix_sort
+    for i in range(0, digits):
+        arr = sorted(arr, key=lambda x: (x % (base**(i+1))) // (base**i))
     return arr
     
 def test():
-    arr = [random.randint(10**0, 10**4) for x in range(1000)]
-    sorted_arr = radix_sort(arr, 5)
+    base = 10
+    digits = 4
+    arr = [random.randint(base**0, base**digits) for x in range(1000)]
+    sorted_arr = radix_sort(arr, digits + 1)
     assert sorted_arr == sorted(arr)
     
 if __name__ == '__main__':
