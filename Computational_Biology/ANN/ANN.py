8ca848629ceca128a44ed7018eda0bb20ae6d2ec
uriyay <uriyay@gmail.com>
2021-02-09 14:07:31 +0200

    Neuron will be able to calculate inputs without InputNeuron, replaced for-loop method with vectors multiplication of weights and inputs

--- Computational_Biology/ANN/ANN.py
+++ Computational_Biology/ANN/ANN.py
@@ -1,15 +1,19 @@
 import logging
+import numpy as np
 
 class Neuron:
     def __init__(self, threshold):
         self.threshold = threshold
         self.inputs = []
+        #float32 is enough
+        self.weights = np.array([], dtype=np.float32)
 
     def connect(self, output, wheight):
         self.output.add_input(self, wheight)
 
     def add_input(self, input_neuron, wheight):
-        self.inputs.append([input_neuron, wheight])
+        self.inputs.append(input_neuron)
+        self.weights = np.append(self.weights, wheight)
 
     def add_inputs(self, *inputs_and_wheights):
         """
@@ -18,29 +22,24 @@ class Neuron:
         for input_neuron, wheight in inputs_and_wheights:
             self.add_input(input_neuron, wheight)
 
+    def _activate_input(self, neuron):
+        if isinstance(neuron, Neuron):
+            return neuron.activate()
+        else:
+            #its a basic type - an input or bias
+            return neuron
+
     def activate(self):
-        result = 0
-        for neuron, wheight in self.inputs:
-            result += neuron.activate() * wheight
+        inputs = np.array([self._activate_input(n) for n in self.inputs])
+        result = np.dot(inputs, self.weights)
         if result >= self.threshold:
             return 1
         return -1
 
-class InputNeuron(Neuron):
-    """
-    A special neuron for input
-    """
-    def __init__(self, value):
-        super().__init__(threshold=None)
-        self.value = value
-
-    def activate(self):
-        return self.value
-
 def test_majority(x1, x2, x3):
-    n1 = InputNeuron(x1)
-    n2 = InputNeuron(x2)
-    n3 = InputNeuron(x3)
+    n1 = x1
+    n2 = x2
+    n3 = x3
     majority_neuron = Neuron(1)
     majority_neuron.add_inputs(
             (n1, 0.8),
@@ -48,6 +47,15 @@ def test_majority(x1, x2, x3):
             (n3, 0.6)
     )
     result = majority_neuron.activate()
+    return int(result > 0)
+
+def test_lot_inputs():
+    import random
+    inputs = [random.randint(0, 100) for x in range(100*100)]
+    weights = [random.randint(0, 100) / 100 for x in range(100*100)]
+    output = Neuron(1)
+    output.add_inputs(*zip(inputs, weights))
+    result = output.activate()
     return int(result > 0)
 
 class Perceptron:
@@ -59,18 +67,17 @@ class Perceptron:
         self.logger.setLevel(log_level)
 
     def _set_inputs(self, x):
-        for idx, inp in enumerate(self.output.inputs):
-            inp[0].value = x[idx]
+        self.output.inputs = x
 
-    def _get_wheights(self):
-        return [x[1] for x in self.output.inputs]
+    def _get_weights(self):
+        return self.output.weights
 
     def fit(self, X, Y, max_iterations=None, learning_rate=1):
         #init the network
         if len(X) == 0:
             raise Exception("zero samples given")
         n = len(X[0])
-        inputs = [InputNeuron(0) for _ in range(n)]
+        inputs = [0 for _ in range(n)]
         wheights = [0 for _ in range(n)]
         self.output = Neuron(threshold=self.threshold)
         self.output.add_inputs(*zip(inputs, wheights))
@@ -99,10 +106,10 @@ class Perceptron:
                         raise Exception("class is invalid (%s)" % (y))
 
                     self.logger.debug("class incorrect (%s instead of %s)" % (x_class, y))
-                    self.logger.debug("current wheights: %s" % (self._get_wheights()))
-                    for idx, inp in enumerate(self.output.inputs):
-                        inp[1] += (x[idx] * sign * learning_rate)
-                    self.logger.debug("updated to new wheights: %s" % (self._get_wheights()))
+                    self.logger.debug("current wheights: %s" % (self._get_weights()))
+                    for idx, _ in enumerate(self.output.weights):
+                        self.output.weights[idx] += (x[idx] * sign * learning_rate)
+                    self.logger.debug("updated to new wheights: %s" % (self._get_weights()))
             if is_perfect_wheighted:
                 is_learning_succeeded = True
                 self.logger.debug("Learning done!")
